b88a02201456d4ef94152d49d846f8d0493e9779
Ibrahim Rather <irather@uoguelph.ca>
2022-10-22 18:54:45 -0400

    Resolve "issue-41-creating initial react app"

---
+++ scheduler-app/src/App.js
@@ -0,0 +1,18 @@
+import './App.css';
+
+function App() {
+  return (
+    <div>
+      <h1>
+        Scheduler
+      </h1>
+
+      <div>
+        <h2>Schedule</h2>
+      </div>
+
+    </div>
+  );
+}
+
+export default App;
