032aa8f5cb1da13cdf19d3e81f645b4e9d430f3c
Ibrahim Rather <ibrahim.s.rather@gmail.com>
2022-10-22 23:32:05 -0400

    adding in example navbar and buttons, adjusting example stylings for homepage

--- scheduler-app/src/App.js
+++ scheduler-app/src/App.js
@@ -14,10 +14,31 @@ function App() {
   }, []);
 
   return (
-    <div className="home-page">
-      <h1>Scheduler</h1>
-      <h2>Schedule</h2>
-      <p>The bean of the day is {profileData}.</p>
+    <div>
+      <nav className="navbar navbar-expand-lg">
+        <div className="container-fluid">
+          <a className="navbar-brand" href="#">Scheduler</a>
+          <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+            <span className="navbar-toggler-icon"></span>
+          </button>
+          <div className="collapse navbar-collapse" id="navbarNavAltMarkup">
+            <div className="navbar-nav">
+              <a className="nav-link active" aria-current="page" href="#">Home</a>
+              <a className="nav-link" href="#">Example Link</a>
+            </div>
+          </div>
+        </div>
+      </nav>
+
+      <div className="home-page">
+        <h2>Schedule</h2>
+        <p>The bean of the day is {profileData}.</p>
+
+        <button type="button" className="btn btn-lg">Click Me!</button>
+        <button type="button" className="btn btn-lg">Click Me!</button>
+        <button type="button" className="btn btn-lg">Click Me!</button>
+      </div>
+
     </div>
   );
 }
