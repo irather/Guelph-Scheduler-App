08a3eef9d85f5a19ad72ccbe81aad9517e2350de
cfitzg05 <cfitzg05@uoguelph.ca>
2022-10-27 21:03:16 -0400

    issue-50 installation script updated

--- uninstall.sh
+++ uninstall.sh
@@ -1,4 +1,30 @@
 sudo nginx -s quit
+
+sudo pip uninstall gunicorn -y
+
+echo
+echo uninstalled gunicorn
+echo
+
+cd scheduler-app/node_modules
+sudo npm uninstall *
+cd ../
+cd ../
+sudo apt-get remove nodejs -y
+sudo apt-get purge nodejs -y
+
+echo 
+echo uninstalled npm
+echo
+
+sudo apt-get remove python3-pip -y
+sudo pip uninstall flask
+sudo pip uninstall python-dotenv
+
+echo
+echo Uninstalled flask & python-dotenv
+echo
+
 sudo apt-get purge nginx nginx-common -y
 echo
 echo Uninstalled nginx
